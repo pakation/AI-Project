c00cd6aef3dfc173f0f089d5b4e79d5c3fc5d90f
pakation <pepepaco20@gmail.com>
2016-10-09 22:24:11 -0500

    Update proyecto1.pl

--- proyecto1.pl
+++ proyecto1.pl
@@ -12,6 +12,45 @@
 %****************************************************************
 
 %****************************************************************
+% Carga y lectura de la base de conocimiento
+%****************************************************************
+
+%KB open and save
+
+open_kb(Route,KB):-
+	open(Route,read,Stream),
+	readclauses(Stream,X),
+	close(Stream),
+	atom_to_term(X,KB).
+
+save_kb(Route,KB):-
+	open(Route,write,Stream),
+	writeq(Stream,KB),
+	close(Stream).
+
+readclauses(InStream,W) :-
+        get0(InStream,Char),
+        checkCharAndReadRest(Char,Chars,InStream),
+	atom_chars(W,Chars).
+
+checkCharAndReadRest(-1,[],_) :- !.  % End of Stream	
+checkCharAndReadRest(end_of_file,[],_) :- !.
+
+checkCharAndReadRest(Char,[Char|Chars],InStream) :-
+        get0(InStream,NextChar),
+        checkCharAndReadRest(NextChar,Chars,InStream).
+
+%compile an atom string of characters as a prolog term
+atom_to_term(ATOM, TERM) :-
+	atom(ATOM),
+	atom_to_chars(ATOM,STR),
+	atom_to_chars('.',PTO),
+	append(STR,PTO,STR_PTO),
+	read_from_chars(STR_PTO,TERM).
+
+:- op(800,xfx,'=>').
+
+%****************************************************************
 % 1a. Extensiones de una class
 %
 % Usage: extension_class(NomClase, ExtensionesPorId, KnowledgeBase)
@@ -563,43 +602,5 @@ elimina_lista_objetos([NomObjeto|C],KB_Original,KB_Nuevo) :-
 	elimina_lista_objetos(C,KB_Original,KB_Aux),elimina_toda_relacion(NomObjeto,KB_Aux,KB_Nuevo).
 elimina_lista_objetos([NomObjeto|_],KB_Original,KB_Nuevo) :-
 	elimina_toda_relacion(NomObjeto,KB_Original,KB_Nuevo).
-	
-%****************************************************************
-% Carga y lectura de la base de conocimiento
-%****************************************************************
 
-%KB open and save
-
-open_kb(Route,KB):-
-	open(Route,read,Stream),
-	readclauses(Stream,X),
-	close(Stream),
-	atom_to_term(X,KB).
-
-save_kb(Route,KB):-
-	open(Route,write,Stream),
-	writeq(Stream,KB),
-	close(Stream).
-
-readclauses(InStream,W) :-
-        get0(InStream,Char),
-        checkCharAndReadRest(Char,Chars,InStream),
-	atom_chars(W,Chars).
-
-checkCharAndReadRest(-1,[],_) :- !.  % End of Stream	
-checkCharAndReadRest(end_of_file,[],_) :- !.
-
-checkCharAndReadRest(Char,[Char|Chars],InStream) :-
-        get0(InStream,NextChar),
-        checkCharAndReadRest(NextChar,Chars,InStream).
-
-%compile an atom string of characters as a prolog term
-atom_to_term(ATOM, TERM) :-
-	atom(ATOM),
-	atom_to_chars(ATOM,STR),
-	atom_to_chars('.',PTO),
-	append(STR,PTO,STR_PTO),
-	read_from_chars(STR_PTO,TERM).
-
-:- op(800,xfx,'=>').
 
