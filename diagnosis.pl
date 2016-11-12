4c9686454c8fecbda6e6303ac19b080bdfef1ab1
Derek <derek@port7e.ca>
2016-11-12 14:50:06 -0600

    eligir mayor solucion

--- diagnosis.pl
+++ diagnosis.pl
@@ -36,8 +36,9 @@ diagnosis(Creencias, Observaciones, Creencias_New, []) :-
 	remove_inconsistencies(Creencias, Observaciones, [], Creencias_Malos, [], Creencias_Limpios),
 	% TODO [] below needs to be a list of shelves
 	generar_explicaciones(Creencias_Malos, Observaciones, [s1,s2,s3], [], Explicaciones),
+	eligir_explicacion(Creencias, Explicaciones, Eligido),
 	nl,
-	write(Explicaciones).
+	write(Eligido).
 
 % Strip out bad creencias
 
@@ -87,3 +88,54 @@ expandir_explicaciones(Item, [Shelf|T], Obvs, Expl_A, Expl_New) :-
 	append(Expl_A, [Item => Shelf], Expl_B),
 	expandir_explicaciones(Item, T, Obvs, Expl_B, Expl_New)
 	.
+
+% test([], Choice_New, Choice_New, Min_New, Min_New).
+% test([Key => Number|T], Choice, Choice_New, Min, Min_New) :-
+% 	(Number @< Min),
+% 	test(T, Key, Choice_New, Number, Min_New)
+% 	;
+% 	test(T, Choice, Choice_New, Min, Min_New).
+
+%
+eligir_explicacion(Creencias, Explicaciones, Eligido) :-
+	do_eligir_explicacion(Creencias, Explicaciones, _, Eligido, 999999, _).
+
+do_eligir_explicacion(Creencias, [], Eligido_New, Eligido_New, Min_New, Min_New).
+do_eligir_explicacion(Creencias, [Explicacion|T], Eligido, Eligido_New, Min, Min_New) :-
+	% Look for shelf count discrepancies
+	delta_count(Creencias, Explicacion, Result),
+	(Result @< Min),
+	% If this solution has less of a discrepancy
+	do_eligir_explicacion(Creencias, T, Explicacion, Eligido_New, Result, Min_New)
+	;
+	% If this solution has more of a discrepancy
+	do_eligir_explicacion(Creencias, T, Eligido, Eligido_New, Min, Min_New).
+
+% Delta count
+%
+% TODO prove that this is is OK for all cases
+%
+% Take creencias. For every item in the explanation
+update_shelf(Shelf, Amount, [], [Shelf => Amount]).
+update_shelf(Shelf, Amount, [Shelf => Count|T], [Shelf => Count_New|T]) :- Count_New is Count + Amount.
+update_shelf(Shelf, Amount, [H|T], [H|T_New]) :- update_shelf(Shelf, Amount, T, T_New).
+
+delta_count(Creencias, Explicaciones, Total) :- 
+	do_delta_count(Creencias, Explicaciones, [], Shelves),
+	do_delta_total(Shelves, 0, Total).
+
+do_delta_total([], Total_New, Total_New).
+do_delta_total([Shelf => Count|T], Total_A, Total_New) :-
+	Total_B is Total_A + abs(Count),
+	do_delta_total(T, Total_B, Total_New).
+
+% Creencias, explicaciones, temp, temp, Output
+do_delta_count(Creencias, [], Shelves_New, Shelves_New).
+do_delta_count(Creencias, [Item => Shelf|T], Shelves_A, Shelves_New) :-
+	% -1 for the shelf that we thought the item was on
+	is_on_shelf(Item, Shelf_Old, Creencias),
+	update_shelf(Shelf_Old, -1, Shelves_A, Shelves_B),
+	% +1 for the shelf that the item is really on
+	update_shelf(Shelf, 1, Shelves_B, Shelves_C),
+	do_delta_count(Creencias, T, Shelves_C, Shelves_New).
+
