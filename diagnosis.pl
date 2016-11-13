c0812f35fd6b631aa99766e4f0f6298838e98261
Derek <derek@port7e.ca>
2016-11-13 12:16:48 -0600

    diagnosis hasta devolver nuevos creencias

--- diagnosis.pl
+++ diagnosis.pl
@@ -1,30 +1,17 @@
 % :- ensure_loaded(proyecto1).
+:- op(800,xfx,'=>').
 
 % TODO deal with observations where shelf is totally empty
 % TODO handle cases where there are no contradicions
+% TODO turn Creencias_New into Acciones (why?)
 
-:- op(800,xfx,'=>').
-
-% -- initial diagnosis
-% take all objects that the shopkeeper mentioned
-% for each object, pair it with where it's supposed to be
-% then generate mover,colocar pairs for each
-% then merge colocars
-%
-% -- normal diagnosis
-% > take initial diagnosis and observations
-% < return new diagnosis
-%
-% for each item in the diagnosis, look in observations for any contradictions
-% if one exists, remove the offending entry to construct a diagnosis free of errors.
-% take this entry and, checking against observations, generate all possible alternatives
-%
-% if no more contradictions exist, take all offending entries and generate all
-% posible permutations. for each, append the "clean" diagnosis, then compare the 
-% number of items per shelf to the expected.
-%
-% choose the permutation that is the closest to expected
-% generate move,colocar pairs for this permutation
+% Creencias, creencias nuevos, observaciones, acciones
+diagnosis(Creencias, Observaciones, Creencias_New, []) :- 
+	remove_inconsistencies(Creencias, Observaciones, [], Creencias_Malos, [], Creencias_Limpios),
+	generar_posibilidades(Creencias_Malos, Observaciones, [s1,s2,s3], Posibilidades),
+	generar_explicaciones(Posibilidades, [], [], Explicaciones),
+	eligir_explicacion(Creencias, Explicaciones, Eligido),
+	aplicar_explicacion(Creencias, Eligido, Creencias_New).
 
 % returns true if the item has been seen on a different shelf, or the item
 % has never been seen but the shelf that the item should be on has been,
@@ -36,20 +23,7 @@ do_contradicts_obs(Item, _, [Item => _|_], _, _). % true, stop
 do_contradicts_obs(Item, Shelf, [_ => Shelf|T], _, Seen_Shelf_New) :- do_contradicts_obs(Item, Shelf, T, true, Seen_Shelf_New).
 do_contradicts_obs(Item, Shelf, [_|T], Seen_Shelf, Seen_Shelf_New) :- do_contradicts_obs(Item, Shelf, T, Seen_Shelf, Seen_Shelf_New).
 
-% Creencias, creencias nuevos, observaciones, acciones
-diagnosis(Creencias, Observaciones, Creencias_New, []) :- 
-	remove_inconsistencies(Creencias, Observaciones, [], Creencias_Malos, [], Creencias_Limpios),
-	generar_posibilidades(Creencias_Malos, Observaciones, [s1,s2,s3], Posibilidades),
-	generar_explicaciones(Posibilidades, [], [], Explicaciones),
-	eligir_explicacion(Creencias, Explicaciones, Eligido),
-	nl,
-	write(Eligido).
-
 % Strip out bad creencias
-
-% Unit tests: 
-%	remove_inconsistencies([a => b], [], [], A, [], B), A=[], B=[a=>b]
-%	remove_inconsistencies([a => b], [a => c], [], A, [], B), A=[a], B=[]
 remove_inconsistencies([], _, Creencias_Malos, Creencias_Malos, Creencias_New, Creencias_New).
 % for each item
 remove_inconsistencies([Item => Shelf|T], Obvs, Creencias_Malos_A, Creencias_Malos, Creencias_A, Creencias_New) :-
@@ -70,7 +44,6 @@ remove_inconsistencies([Item => Shelf|T], Obvs, Creencias_Malos_A, Creencias_Mal
 has_seen_shelf(Shelf, [_ => Shelf|_]).
 has_seen_shelf(Shelf, [_|T]) :- has_seen_shelf(Shelf, T).
 
-% stricter than is_consistent
 is_on_shelf(Item, Shelf, [Item => Shelf|_]).
 is_on_shelf(Item, Shelf, [_|T]) :- is_on_shelf(Item, Shelf, T).
 
@@ -100,6 +73,8 @@ do_generar_posibilidades(Item, [Shelf|T], Obvs, Expl_A, Expl_New) :-
 	do_generar_posibilidades(Item, T, Obvs, Expl_B, Expl_New)
 	.
 
+% given a 2d array of posibilities, expand it into a flat 1d array of
+% all permutations. So [[1,2],[3,4]] should turn into [[1,3],[1,4],[2,3],2,4]]
 generar_explicaciones([[]], Cadena, Results_New, Results_New).
 generar_explicaciones([], Cadena, Results_A, Results_New) :-
 	append(Results_A, [Cadena], Results_New).
@@ -109,18 +84,13 @@ generar_explicaciones([[Item => Shelf|T]|T_2], Cadena_A, Results_A, Results_New)
 	generar_explicaciones(T_2, Cadena_B, Results_A, Results_B),
 	generar_explicaciones([T|T_2], Cadena_A, Results_B, Results_New).
 
-% test([], Choice_New, Choice_New, Min_New, Min_New).
-% test([Key => Number|T], Choice, Choice_New, Min, Min_New) :-
-% 	(Number @< Min),
-% 	test(T, Key, Choice_New, Number, Min_New)
-% 	;
-% 	test(T, Choice, Choice_New, Min, Min_New).
-
-%
+% choose an explanation given a set of explanations
 eligir_explicacion(_, [Item => Shelf], [Item => Shelf]). % Explanation with only one option, always choose that option
 eligir_explicacion(Creencias, Explicaciones, Eligido) :-
 	do_eligir_explicacion(Creencias, Explicaciones, _, Eligido, 999999, _).
 
+% compute the delta between this explanation and the ideal or expected
+% and select the option with minimum delta
 do_eligir_explicacion(_, [], Eligido_New, Eligido_New, Min_New, Min_New).
 do_eligir_explicacion(Creencias, [Explicacion|T], Eligido, Eligido_New, Min, Min_New) :-
 	% Look for shelf count discrepancies
@@ -133,13 +103,12 @@ do_eligir_explicacion(Creencias, [Explicacion|T], Eligido, Eligido_New, Min, Min
 	do_eligir_explicacion(Creencias, T, Eligido, Eligido_New, Min, Min_New).
 
 % Delta count
-%
 % TODO prove that this is is OK for all cases
-%
-% Take creencias. For every item in the explanation
 update_shelf(Shelf, Amount, [], [Shelf => Amount]).
-update_shelf(Shelf, Amount, [Shelf => Count|T], [Shelf => Count_New|T]) :- Count_New is Count + Amount.
-update_shelf(Shelf, Amount, [H|T], [H|T_New]) :- update_shelf(Shelf, Amount, T, T_New).
+update_shelf(Shelf, Amount, [Shelf => Count|T], [Shelf => Count_New|T]) :- 
+	Count_New is Count + Amount.
+update_shelf(Shelf, Amount, [H|T], [H|T_New]) :- 
+	update_shelf(Shelf, Amount, T, T_New).
 
 delta_count(Creencias, Explicaciones, Total) :- 
 	do_delta_count(Creencias, Explicaciones, [], Shelves),
@@ -160,3 +129,16 @@ do_delta_count(Creencias, [Item => Shelf|T], Shelves_A, Shelves_New) :-
 	update_shelf(Shelf, 1, Shelves_B, Shelves_C),
 	do_delta_count(Creencias, T, Shelves_C, Shelves_New).
 
+% apply an explanation to a set of beliefs. The explanation is an incomplete
+% set [item1 => shelf1], that is some items are not defined. The belief is a 
+% complete set.
+aplicar_explicacion(Creencias_New, [], Creencias_New).
+aplicar_explicacion(Creencias_A, [Item => Shelf|T], Creencias_New) :-
+	do_aplicar_explicacion(Item, Shelf, Creencias_A, Creencias_B),
+	aplicar_explicacion(Creencias_B, T, Creencias_New).
+
+do_aplicar_explicacion(Item, Shelf, [], [Item => Shelf]).
+do_aplicar_explicacion(Item, Shelf, [Item => _|T], [Item => Shelf_Old|T]) :-
+	Shelf_Old = Shelf.
+do_aplicar_explicacion(Item, Shelf, [H|T], [H|T_New]) :-
+	do_aplicar_explicacion(Item, Shelf, T, T_New).
